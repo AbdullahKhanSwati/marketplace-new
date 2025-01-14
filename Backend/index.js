10188ffdfaa9015bf713c52813990878f42b114a
AbdullahKhanSwati <101318760+AbdullahKhanSwati@users.noreply.github.com>
2025-01-14 21:42:46 +0500

    updating ....

--- Backend/index.js
+++ Backend/index.js
@@ -29,14 +29,7 @@ connectDb();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors(
-{
-  origin: ["https://deploy-mern-1whq.vercel.app"] , 
-  methods : ["POST" , "GET"],
-  credentials:true
-}
-
-));
+app.use(cors());
 // app.use(formidableMiddleware());
 // app.use(bodyParser.json());
 // app.use(bodyParser.json({ limit: '50mb' }));
